f5662b62eba0e0895ddd1ec19bf24c3977862fb5
Erin Wilcox <erin.janae.anderson@gmail.com>
2017-11-24 07:33:19 -0700

    bootstrap site
    Add bootstrap site

---
+++ js/stylish-portfolio.js
@@ -0,0 +1,184 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+  // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+  });
+
+  //#to-top button appears after scrolling
+  var fixed = false;
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 250) {
+      if (!fixed) {
+        fixed = true;
+        $('#to-top').show("slow", function() {
+          $('#to-top').css({
+            position: 'fixed',
+            display: 'block'
+          });
+        });
+      }
+    } else {
+      if (fixed) {
+        fixed = false;
+        $('#to-top').hide("slow", function() {
+          $('#to-top').css({
+            display: 'none'
+          });
+        });
+      }
+    }
+  });
+
+})(jQuery); // End of use strict
+
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(event) {
+  var that = $(this);
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function(event) {
+  var that = $(this);
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
+}
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
+
+
+
+var slideIndex = 1;
+var slideType = "engagement";
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName(slideType);
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+}
+
+function showWeddingGallery() {
+  var weddingGalleryVisibility = true;
+}
+
+function setupSlides() {
+  document.onkeydown = function (e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            plusSlides(-1);
+            // left arrow
+            break;
+        case 'ArrowRight':
+            plusSlides(1);
+           // right arrow
+    }
+  };
+}
+
+function tearDownSlides() {
+  document.onkeydown = function(e) {
+  }
+}
+
+function onShowEngagement() {
+  setupSlides();
+  $("#engagementOverlay").modal({backdrop: true});  
+  slideType = "engagement";
+  showSlides(slideIndex);
+}
+
+function onShowWedding() {
+  setupSlides();
+  $("#weddingOverlay").modal({backdrop: true});
+  slideType = "wedding";
+  showSlides(slideIndex);
+}
+
+function onShowFamily() {
+  setupSlides();
+  $("#familyOverlay").modal({backdrop: true});
+  slideType = "family";
+  showSlides(slideIndex);
+}
+
+function onShowPortrait() {
+  setupSlides();
+  $("#portraitOverlay").modal({backdrop: true});
+  slideType = "portrait";
+  showSlides(slideIndex);
+}
+
+function engagementOff() {
+  tearDownSlides();
+}
+
+function weddingOff() {
+  tearDownSlides();
+}
+
+function familyOff() {
+  tearDownSlides();
+}
+
+function portraitOff() {
+  tearDownSlides();
+}
+
+function on(category) {
+  slideType = category;
+  showSlides(slideIndex);
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
